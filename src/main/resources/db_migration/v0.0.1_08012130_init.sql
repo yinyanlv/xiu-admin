1bfbc2d378651c50baa3a8462fd8ffce35cfeb15
yinyanlv <yanlv.yin@gmail.com>
2018-09-12 17:50:44 +0800

    edit sql

--- src/main/resources/db_migration/v0.0.1_08012130_init.sql
+++ src/main/resources/db_migration/v0.0.1_08012130_init.sql
@@ -13,15 +13,11 @@ CREATE TABLE user_role (
   sort TINYINT UNSIGNED NOT NULL
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8;
 
-INSERT INTO user_role (code, name, sort) VALUES ('ordinary', '普通用户', 1), ('admin', '管理员', 2);
-
 CREATE TABLE user_status (
   code CHAR(10) PRIMARY KEY NOT NULL,
   name CHAR(10) NOT NULL,
   sort TINYINT UNSIGNED NOT NULL
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8;
-
-INSERT INTO user_status (code, name, sort) VALUES ('normal', '正常', 1), ('forbidden', '禁用', 2);
 
 CREATE TABLE user (
   id CHAR(32) PRIMARY KEY NOT NULL,
@@ -31,8 +27,8 @@ CREATE TABLE user (
   email VARCHAR(30) NOT NULL,
   phone CHAR(11),
   qq VARCHAR(15),
-  role CHAR(10) NOT NULL DEFAULT 'ordinary',
-  status CHAR(10) NOT NULL DEFAULT 'normal',
+  role VARCHAR(10) NOT NULL DEFAULT 'ordinary',
+  status VARCHAR(10) NOT NULL DEFAULT 'normal',
   create_by VARCHAR(20) NOT NULL,
   create_time DATETIME NOT NULL DEFAULT NOW(),
   update_by VARCHAR(20) NOT NULL,
@@ -41,10 +37,8 @@ CREATE TABLE user (
   UNIQUE KEY username (username)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8;
 
-INSERT INTO user (id, username, password, nickname, email, role, create_by, update_by) VALUES ('abc', 'admin', '111111', '超级管理员', '1761869346@qq.com', 'admin', 'admin', 'admin');
-
 CREATE TABLE category (
-  id VARCHAR(20) PRIMARY KEY NOT NULL,
+  id CHAR(32) PRIMARY KEY NOT NULL,
   parent_id VARCHAR(20),
   name VARCHAR(20) NOT NULL,
   keywords VARCHAR(200),
@@ -52,7 +46,7 @@ CREATE TABLE category (
   content VARCHAR(20000),
   banner VARCHAR(100) COMMENT '分类banner地址',
   thumbnail VARCHAR(100) COMMENT '分类缩略图地址',
-  status TINYINT UNSIGNED NOT NULL DEFAULT 0 COMMENT '0: 正常, 1: 不显示, 2: 不可链接',
+  status VARCHAR(10) NOT NULL DEFAULT 'normal' COMMENT 'normal: 正常, hide: 不显示',
   sort TINYINT UNSIGNED,
   create_by VARCHAR(20) NOT NULL,
   create_time DATETIME NOT NULL DEFAULT NOW(),
@@ -61,14 +55,14 @@ CREATE TABLE category (
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8;
 
 CREATE TABLE article (
-  id CHAR(20) PRIMARY KEY NOT NULL,
+  id CHAR(32) PRIMARY KEY NOT NULL,
   title VARCHAR(200) NOT NULL,
   keywords VARCHAR(200),
   description VARCHAR(200),
   content VARCHAR(20000) NOT NULL,
   banner VARCHAR(100) COMMENT '文章banner地址',
   thumbnail VARCHAR(100) COMMENT '文章缩略图地址',
-  status TINYINT UNSIGNED NOT NULL DEFAULT 0 COMMENT '0: 正常, 1: 不显示',
+  status VARCHAR(10) NOT NULL DEFAULT 'normal' COMMENT 'normal: 正常, hide: 不显示',
   sort INT UNSIGNED,
   category_id VARCHAR(20) NOT NULL,
   create_by VARCHAR(20) NOT NULL,
@@ -79,14 +73,14 @@ CREATE TABLE article (
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8;
 
 CREATE TABLE comment (
-  id CHAR(20) PRIMARY KEY NOT NULL,
+  id CHAR(32) PRIMARY KEY NOT NULL,
   nickname VARCHAR(20) NOT NULL,
   ip CHAR(20) NOT NULL,
   email VARCHAR(30),
   phone CHAR(11),
   qq VARCHAR(15),
   conent VARCHAR(200) NOT NULL,
-  status TINYINT UNSIGNED NOT NULL DEFAULT 0 COMMENT '0: 正常, 1: 不显示',
+  status VARCHAR(10) NOT NULL DEFAULT 'normal' COMMENT 'normal: 正常, hide: 不显示',
   article_id VARCHAR(20) NOT NULL,
   create_by VARCHAR(20) NOT NULL,
   create_time DATETIME NOT NULL DEFAULT NOW(),
@@ -116,8 +110,15 @@ CREATE TABLE log (
   id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
   username VARCHAR(20) NOT NULL,
   ip CHAR(20) NOT NULL,
-  operation TINYINT NOT NULL DEFAULT 0 COMMENT '0: 新增, 1: 修改, 2: 删除',
-  type TINYINT NOT NULL DEFAULT 0 COMMENT '0: 分类, 1: 文章, 2: 评论',
+  operation VARCHAR(10) NOT NULL DEFAULT 'create' COMMENT 'create: 新增, edit: 修改, delete: 删除',
+  type VARCHAR(10) NOT NULL DEFAULT 'category' COMMENT 'category: 分类, article: 文章, comment: 评论',
   content VARCHAR(200) NOT NULL,
   create_time DATETIME NOT NULL DEFAULT NOW()
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8;
+
+
+INSERT INTO user_role (code, name, sort) VALUES ('ordinary', '普通用户', 1), ('admin', '管理员', 2);
+
+INSERT INTO user_status (code, name, sort) VALUES ('normal', '正常', 1), ('forbidden', '禁用', 2);
+
+INSERT INTO user (id, username, password, nickname, email, role, create_by, update_by) VALUES ('abc', 'admin', '111111', '超级管理员', '1761869346@qq.com', 'admin', 'admin', 'admin');
